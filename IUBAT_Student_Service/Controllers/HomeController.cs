84e03e2264bde315e89942ea0fe1ebae82900253
shakkhorpaul50-ai <shakkhorpaul50@gmail.com>
2026-09-04 19:58:50 +0600

    feat: replace Privacy with full About Us (IUBAT info + developer Shakkhor Paul github https://github.com/shakkhorpaul50-ai/) - add Home/About route, keep Privacy as alias, update nav/footer

--- IUBAT_Student_Service/Controllers/HomeController.cs
+++ IUBAT_Student_Service/Controllers/HomeController.cs
@@ -16,6 +16,11 @@ namespace IUBAT_Student_Service.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
